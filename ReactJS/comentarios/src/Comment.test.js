4b7a2c5f8705cf562b537b4d8231fd5a39c9e896
jhonatan.grymuza <jhonyzanatta@hotmail.com>
2019-07-12 01:17:34 -0300

    adiconado arquivos de testes com enzyme

---
+++ ReactJS/comentarios/src/Comment.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Comment from './Comment'
+import { render } from 'enzyme'
+
+import Adapter from 'enzyme-adapter-react-16'
+import Enzyme from 'enzyme'
+Enzyme.configure({ adapter: new Adapter() })
+
+
+it('should render', () => {
+    const c = {
+        comment:'test'
+    }
+
+    const wrapper = render(<Comment c={c}/>)
+    expect(wrapper.text()).toBe('Comentário:test')
+})
