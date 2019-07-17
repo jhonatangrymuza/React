93b69a55ca499fa7ad126b561dd444b21edd45cd
jhonatan.grymuza <jhonyzanatta@hotmail.com>
2019-07-17 00:15:00 -0300

    react-native-meu-ip

---
+++ ReactNative/meuip/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View,Button,Image } from 'react-native';
+import logo from './assets/logo.png'
+
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.findMyIp = this.findMyIp.bind(this)
+
+    this.state ={
+      data:''
+    }
+  }
+  async findMyIp(){
+    this.setState({
+      data:"descobrindo IP..."
+    })
+    const ip = await fetch('http://httpbin.org/ip')
+    const data = await ip.json()
+    console.warn( data);
+    this.setState({
+      
+      data:data.origin
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <Image source={logo}/>
+          <Text style={styles.ip}>{this.state.data}</Text>
+          <Button  title='Descobrir meu IP' onPress={this.findMyIp}/>
+        </View>
+        <View style={styles.footer}>
+          <Text style={styles.made}>feito por jhonatan grymuza </Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2F2336',
+    alignItems: 'stretch',
+    justifyContent: 'space-around',
+  },
+  body: {
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  ip:{
+    color:'white',
+    paddingTop:20,
+    paddingBottom:20
+  },
+  made:{
+    color:'white',
+    textAlign:'center'
+  },
+  footer:{
+    paddingTop:10,
+    paddingBottom:10
+  }
+
+});
